14dcbb733ed2810097f1674f0cb9187f340a6c20
Wook Song <wook16.song@samsung.com>
2022-08-17 17:37:39 +0900

    Bronze: Add a solution for 1284 (Bronze 3)

---
+++ 1284/1284.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+constexpr int MAX_LEN = 5;
+
+int main() {
+    char buf[MAX_LEN];
+
+    while (std::cin.getline(buf, MAX_LEN)) {
+        std::string num = std::string(buf);
+        uint32_t width;
+
+        if (num == "0")
+            break;
+
+        width = 0;
+        for (auto iter = num.rbegin(); iter != num.rend(); ++iter) {
+            switch (*iter) {
+            case '1':
+                width += 2;
+                break;
+            case '0':
+                width += 4;
+                break;
+            default:
+                width += 3;
+            }
+        }
+        width += 2;
+        if (num.length() > 1) {
+            width += (num.length() - 1);
+        }
+        printf("%u\n", width);
+    }
+
+    return 0;
+}
