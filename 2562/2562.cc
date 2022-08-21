7b2594b9c7da3a15ed2ecb608711d30ff798007b
Wook Song <wook16.song@samsung.com>
2022-08-21 22:39:30 +0900

    Bronze: Add a solution for 2562 (Bronze 3)
    I just did my homework:(

---
+++ 2562/2562.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+constexpr uint32_t MAX_CNTS_NUMS = 9;
+int main() {
+    uint32_t idx;
+    int32_t max = -1;
+
+    // freopen("input.txt", "r", stdin);
+
+    for (uint32_t i = 1; i <= MAX_CNTS_NUMS; ++i) {
+        int32_t num;
+
+        std::cin >> num;
+        if (num > max) {
+            max = num;
+            idx = i;
+        }
+    }
+
+    std::cout << max << "\n";
+    std::cout << idx << "\n";
+
+    return 0;
+}
