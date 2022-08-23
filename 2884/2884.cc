7dc99eac553e2a4898ff3b87e7a497f54ab38450
Wook Song <wook16.song@samsung.com>
2022-08-23 17:59:18 +0900

    Bronze: Add a solution for the Bronze 3 problem, 2884

---
+++ 2884/2884.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int32_t H, M;
+
+    std::cin >> H >> M;
+
+    M -= 45;
+    if (M < 0) {
+        M += 60;
+        H--;
+    }
+
+    if (H < 0) {
+        H += 24;
+    }
+
+    std::cout << H << " " << M << "\n";
+    return 0;
+}
