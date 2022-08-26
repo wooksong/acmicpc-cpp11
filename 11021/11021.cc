82dce77bb0f89887f31507685670bb77f1d75030
Wook Song <wook16.song@samsung.com>
2022-08-27 08:39:39 +0900

    Bronze: Add solutions for two easy problems
    Added 11021 and 11022, which are Bronze 5 problems.

---
+++ 11021/11021.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    uint32_t T, A, B;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> T;
+    for (uint32_t i = 0; i < T; ++i) {
+        std::cin >> A >> B;
+        std::cout << "Case #" << i + 1 << ": " << A + B <<"\n";
+    }
+    return 0;
+}
---
+++ 11022/11022.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    uint32_t T, A, B;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> T;
+    for (uint32_t i = 0; i < T; ++i) {
+        std::cin >> A >> B;
+        std::cout << "Case #" << i + 1 << ": ";
+        std::cout << A << " + " << B << " = " << A + B <<"\n";
+    }
+    return 0;
+}
