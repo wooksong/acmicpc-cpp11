67781ba0b5882b9c1a5d05b7306bfd3aa7fdb121
Wook Song <wook16.song@samsung.com>
2022-08-28 12:44:21 +0900

    Bronze: Add a solution for Bronze 1 problem, 1037

---
+++ 1037/1037.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    uint32_t num_divisors, min, max;
+
+    min = UINT32_MAX;
+    max = 1;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> num_divisors;
+    std::vector<uint32_t> divisors = std::vector<uint32_t>(num_divisors);
+
+    for (uint32_t i = 0; i < num_divisors; ++i) {
+        std::cin >> divisors[i];
+
+        if (min > divisors[i]) {
+            min = divisors[i];
+        }
+
+        if (max < divisors[i]) {
+            max = divisors[i];
+        }
+    }
+
+    std::cout << min * max << "\n";
+}
