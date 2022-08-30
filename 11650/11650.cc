4da3bd5bb440c8b77986530b5a0ff351b3acaac5
Wook Song <wook16.song@samsung.com>
2022-08-30 13:33:10 +0900

    Silver: Add solutions for 10814, 11650, and 11651
    Added solutions for 10814, 11650, and 11651, which are the remaining
    Silver 5 problems in Class 2.

---
+++ 11650/11650.cc
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+struct Point {
+    int32_t x;
+    int32_t y;
+
+    Point() {
+        this->x = 0;
+        this->y = 0;
+    }
+
+    Point(int32_t x, int32_t y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    void dump() { std::cout << this->x << " " << this->y << "\n"; }
+    friend bool operator<(Point &l, Point &r);
+    friend bool operator>(Point &l, Point &r);
+    friend bool operator==(Point &l, Point &r);
+};
+
+int main() {
+    size_t N;
+    std::vector<Point> points = std::vector<Point>();
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> N;
+    for (size_t i = 0; i < N; ++i) {
+        int32_t x, y;
+        Point p;
+
+        std::cin >> x >> y;
+        p = Point(x, y);
+        points.push_back(p);
+    }
+
+    std::sort(points.begin(), points.end());
+    for (auto &p : points) {
+        p.dump();
+    }
+
+    return 0;
+}
+
+bool operator<(Point &l, Point &r) {
+    if (l.x < r.x)
+        return true;
+    if (l.x == r.x)
+        return (l.y < r.y);
+    return false;
+}
+
+bool operator>(Point &l, Point &r) { return !(l < r); }
+bool operator==(Point &l, Point &r) { return l.x == r.x && l.y == r.y; }
---
+++ 11651/11651.cc
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+constexpr int32_t INVALID_VAL = -100001;
+struct Point {
+    int32_t x;
+    int32_t y;
+
+    Point() {
+        this->x = INVALID_VAL;
+        this->y = INVALID_VAL;
+    }
+    Point(int32_t x, int32_t y) {
+        this->x = x;
+        this->y = y;
+    };
+
+    friend bool operator<(Point &l, Point &r);
+    friend bool operator>(Point &l, Point &r);
+};
+
+int main() {
+    uint32_t N;
+    std::vector<Point> points;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> N;
+    for (uint32_t i = 0; i < N; ++i) {
+        Point tmp;
+
+        std::cin >> tmp.x >> tmp.y;
+        points.push_back(tmp);
+    }
+
+    std::sort(points.begin(), points.end());
+    for (auto &p : points) {
+        std::cout << p.x << " " << p.y << "\n";
+    }
+
+    return 0;
+}
+
+bool operator<(Point &l, Point &r) {
+    if (l.y < r.y)
+        return true;
+    if (l.y == r.y)
+        return (l.x < r.x);
+    return false;
+}
+
+bool operator>(Point &l, Point &r) { return !(l < r); }
