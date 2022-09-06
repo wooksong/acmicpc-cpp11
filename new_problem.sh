e141e7774fe8e2d77f1f9d000f6841dbd1cc9da5
Wook Song <wook16.song@samsung.com>
2022-09-06 17:47:14 +0900

    Util: Add a basic template for the solution file

--- new_problem.sh
+++ new_problem.sh
@@ -4,6 +4,6 @@ mkdir -p $1
 cp ./misc/meson.build.in $1/meson.build
 sed -i $([[ $OSTYPE == 'darwin'* ]] && echo ".bak") "s|@NUMBER@|'$1'|g" $1/meson.build
 rm -rf $1/meson.build.bak
-touch $1/$1.cc
+cp ./misc/main.cc.in $1/$1.cc
 echo "subdir('$1')" >> ./meson.build
 
