049d4ed57b553efc9581c2c88ef4e15ad94c7491
Wook Song <wook16.song@samsung.com>
2022-09-06 21:20:23 +0900

    Silver: Add solutions for 17219 and 2090
    Added solutions for two Silver 4 problems, 17219 and 2090

---
+++ 2090/2090.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+int64_t calc_gcd(int64_t l, int64_t r);
+int64_t calc_lcm(int64_t l, int64_t r);
+
+int main() {
+    int64_t N, denominator, sum, reduction;
+    std::vector<int64_t> v;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> N;
+
+    for (int64_t i = 0; i < N; ++i) {
+        int64_t tmp;
+
+        std::cin >> tmp;
+        v.push_back(tmp);
+    }
+
+    denominator = v[0];
+    for (int64_t i = 1; i < N; ++i) {
+        denominator = calc_lcm(denominator, v[i]);
+    }
+
+    sum = 0;
+    for (int64_t i = 0; i < N; ++i) {
+        sum += (denominator / v[i]);
+    }
+
+    reduction = calc_gcd(denominator, sum);
+    denominator /= reduction;
+    sum /= reduction;
+
+    std::cout << denominator << "/" << sum << "\n";
+
+    return 0;
+}
+
+int64_t calc_gcd(int64_t l, int64_t r) {
+    if (l < r)
+        std::swap(l, r);
+    if (l % r == 0)
+        return r;
+
+    return calc_gcd(r, l % r);
+}
+
+int64_t calc_lcm(int64_t l, int64_t r) { return l / calc_gcd(l, r) * r; }
