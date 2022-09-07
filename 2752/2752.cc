426258a5cbf9a821a468c48202bc37dd514eb108
Wook Song <wook16.song@samsung.com>
2022-09-07 14:05:54 +0900

    Bronze: Add a solution for 2752 (Bronze V)

---
+++ 2752/2752.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+constexpr size_t CNT_NUMS = 3;
+
+int32_t nums[CNT_NUMS];
+
+int main() {
+    int32_t min, max, the_other = 0;
+
+    min = INT32_MAX;
+    max = -1;
+
+    // freopen("input.txt", "r", stdin);
+
+    std::cin >> nums[0] >> nums[1] >> nums[2];
+    for (size_t i = 0; i < CNT_NUMS; ++i) {
+        if (nums[i] > max) {
+            max = nums[i];
+        }
+
+        if (nums[i] < min) {
+            min = nums[i];
+        }
+    }
+
+    for (size_t i = 0; i < CNT_NUMS; ++i) {
+        if (nums[i] != max && nums[i] != min) {
+            the_other = nums[i];
+            break;
+        }
+    }
+
+    std::cout << min << " " << the_other << " " << max << "\n";
+
+    return 0;
+}
