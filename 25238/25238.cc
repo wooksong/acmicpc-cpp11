b5b76c9b3b576bd904a56775891d22f524e85b46
Wook Song <wook16.song@samsung.com>
2022-09-09 22:21:42 +0900

    Bronze: Add a solution for 25238
    Added a solution for a Bronze V problem, 25238.

---
+++ 25238/25238.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int def, antidef;
+    int ans = 1;
+
+    std::cin >> def >> antidef;
+
+    if ((def * (100 - antidef)) >= 10000) {
+        ans = 0;
+    }
+
+    std::cout << ans << "\n";
+
+    return 0;
+}
