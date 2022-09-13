d5899c54a45d6d331c7d036c1784a9235f80f4ef
Wook Song <wook16.song@samsung.com>
2022-09-13 15:27:27 +0900

    Silver: Add solutions for 1654, 2805, and 17264
    Added solutions for two Silver II problems (1654 and 2805) and a Silver
    III problem, 17264.

---
+++ 17264/17264.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+constexpr uint64_t HASH_BUCKET = 100007;
+constexpr uint64_t HASH_MUL = 31;
+
+uint64_t gen_hash(std::string str);
+
+int main() {
+    int N, P;
+    int score, score_w, score_l, score_goal;
+    std::vector<char> hash_table;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> N >> P;
+    std::cin >> score_w >> score_l >> score_goal;
+
+    hash_table = std::vector<char>(HASH_BUCKET, 'N');
+
+    for (int i = 0; i < P; ++i) {
+        std::string p_name;
+        char p_res;
+
+        std::cin >> p_name >> p_res;
+        hash_table[gen_hash(p_name)] = p_res;
+    }
+
+    score = 0;
+    for (int i = 0; i < N; ++i) {
+        std::string p_name;
+        uint64_t hash;
+        std::cin >> p_name;
+        hash = gen_hash(p_name);
+        switch (hash_table[hash]) {
+        case 'W':
+            score += score_w;
+            break;
+        default:
+        case 'L':
+            score -= score_l;
+            if (score < 0) {
+                score = 0;
+            }
+            break;
+        }
+        if (score >= score_goal) {
+            std::cout << "I AM NOT IRONMAN!!\n";
+            break;
+        }
+    }
+
+    if (score < score_goal) {
+        std::cout << "I AM IRONMAN!!\n";
+    }
+
+    return 0;
+}
+
+uint64_t gen_hash(std::string str) {
+    uint64_t hash;
+    uint64_t mul;
+
+    hash = 0;
+    mul = 1;
+    for (auto iter = str.begin(); iter != str.end(); ++iter) {
+        hash += ((*iter - 'A') * mul);
+        mul *= HASH_MUL;
+        mul %= HASH_BUCKET;
+        hash %= HASH_BUCKET;
+    }
+
+    return hash;
+}
---
+++ 2805/2805.cc
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+uint64_t binary_search(std::vector<uint64_t> &v, uint64_t min, uint64_t max,
+                       uint64_t target);
+
+int main() {
+    std::vector<uint64_t> trees;
+    uint64_t N, M, ans;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> N >> M;
+    trees = std::vector<uint64_t>(N);
+    for (uint64_t i = 0; i < N; ++i) {
+        std::cin >> trees[i];
+    }
+
+    std::sort(trees.begin(), trees.end());
+    ans = binary_search(trees, 0, trees.back() + 1, M);
+
+    std::cout << ans << "\n";
+    return 0;
+}
+
+uint64_t binary_search(std::vector<uint64_t> &v, uint64_t min, uint64_t max,
+                       uint64_t target) {
+    uint64_t mid;
+    uint64_t sum;
+
+    sum = 0;
+    if ((max - min) <= 1)
+        return min;
+    mid = (max + min) / 2;
+
+    for (auto iter = v.begin(); iter != v.end(); ++iter) {
+        if (mid < *iter) {
+            sum += (*iter - mid);
+        }
+    }
+
+    if (sum < target) {
+        return binary_search(v, min, mid, target);
+    } else if (sum > target) {
+        return binary_search(v, mid, max, target);
+    } else {
+        return mid;
+    }
+}
