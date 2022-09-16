217ade9f0f5a947e04de53a43eefb0a6a4792d1a
Wook Song <wook16.song@samsung.com>
2022-09-16 15:14:08 +0900

    Bronze: Add solutions for 1110 and 25304
    Added solutions for two Bronze problems, 1110 (Bronze I) and 25304
    (Bronze V).

---
+++ 1110/1110.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+uint32_t sum_each_digit(uint32_t num);
+
+int main() {
+    uint32_t N, cnt, new_N;
+
+    std::cin >> N;
+
+    cnt = 0;
+    new_N = N;
+    while (true) {
+        new_N = new_N % 10 * 10 + sum_each_digit(new_N) % 10;
+        cnt++;
+        if (new_N == N) break;
+    }
+    std::cout << cnt << "\n";
+    return 0;
+}
+
+uint32_t sum_each_digit(uint32_t n)
+{
+    uint32_t sum = 0;
+
+    while (n != 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    return sum;
+}
---
+++ 25304/25304.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    uint32_t X, N, a, b, sum;
+
+    freopen("input.txt", "r", stdin);
+
+    std::cin >> X;
+    std::cin >> N;
+
+    sum = 0;
+    for (uint32_t i = 0; i < N; ++i) {
+        std::cin >> a >> b;
+        sum += (a * b);
+    }
+
+    if (sum == X)
+        std::cout << "Yes\n";
+    else
+        std::cout << "No\n";
+    return 0;
+}
