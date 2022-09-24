6efbe0a2c29fd7d631758ac3f3b5ed8129cb5375
Wook Song <wook16.song@samsung.com>
2022-09-24 18:26:42 +0900

    Greedy: Silver: Add a solution for 1105

---
+++ 1105/1105.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int to_num(std::string s);
+int main() {
+    std::string L, R;
+    int cnt = 0;
+
+    // freopen("input.txt", "r", stdin);
+
+    std::cin >> L >> R;
+    if (L.length() == R.length()) {
+        for (size_t i = 0; i < L.length(); ++i) {
+            if ((L[i] == R[i]) && (L[i] == '8')) {
+                cnt++;
+            } else if (L[i] != R[i]) {
+                break;
+            }
+        }
+    }
+
+    std::cout << cnt << "\n";
+
+    return 0;
+}
