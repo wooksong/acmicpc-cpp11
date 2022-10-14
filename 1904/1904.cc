5cd942f91b14f9c2d4ca546b813a5f09380e17fd
Wook Song <wook16.song@samsung.com>
2022-10-14 15:25:43 +0900

    DP: Silver: Add solutions for three DP problems
    Added solutions for 1904, 11727, and 17626 (Silver III), which are DP
    problems.

---
+++ 1904/1904.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+constexpr int MAX_N = 1000001;
+constexpr int DIVISOR = 15746;
+
+int main() {
+    int dp[MAX_N];
+    int N;
+
+    std::cin >> N;
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= N; ++i) {
+        dp[i] = (dp[i - 1] % DIVISOR) + (dp[i - 2] % DIVISOR);
+        dp[i] %= DIVISOR;
+    }
+
+    std::cout << dp[N] << "\n";
+
+    return 0;
+}
