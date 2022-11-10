065a5fc5f86b026693127be947fa04dfc246575e
Wook Song <wook16.song@samsung.com>
2022-11-10 12:44:44 +0900

    Silver: Add a solution for 11053 (Silver II)

---
+++ 11053/11053.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int ans = INT32_MIN;
+    std::vector<int> seq;
+    std::vector<int> lis;
+    int len_seq;
+
+    // freopen("input.txt", "r", stdin);
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cin >> len_seq;
+
+    seq = std::vector<int>(len_seq);
+    for (int i = 0; i < len_seq; ++i) {
+        std::cin >> seq[i];
+    }
+    lis = std::vector<int>(len_seq, 1);
+    for (int i = 0; i < len_seq; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (seq[i] > seq[j]) {
+                lis[i] = std::max(lis[i], lis[j] + 1);
+            }
+        }
+
+        ans = std::max(ans, lis[i]);
+    }
+
+    std::cout << ans << "\n";
+    return 0;
+}
