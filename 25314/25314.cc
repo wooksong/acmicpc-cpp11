455699ef5075545b9aa905235c2b5c59505fd208
Wook Song <wook16.song@samsung.com>
2022-12-05 14:41:56 +0900

    Bronze: Add a solution for 25314 (Bronze V)

---
+++ 25314/25314.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+constexpr int WORD = 4;
+
+int main() {
+    int num_bytes;
+    int num_words;
+
+    // freopen("input.txt", "r", stdin);
+
+    std::cin >> num_bytes;
+    num_words = num_bytes / WORD;
+
+    for (int i = 0; i < num_words; ++i) {
+        std::cout << "long ";
+    }
+
+    std::cout << "int\n";
+
+    return 0;
+}
