ac21f4f261cb98af0d92490dbd6c32cd13f2fc72
Wook Song <wook16.song@samsung.com>
2022-12-06 12:03:24 +0900

    Bronze: Add solutions for 2750, 5622, and 25305 (Bronze II)

---
+++ 25305/25305.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> tmp_arr;
+
+void merge(std::vector<int> &v, int idx_s, int idx_m, int idx_e);
+void sort(std::vector<int> &v, int idx_s, int idx_e);
+
+int main() {
+    int N, k;
+    std::vector<int> scores;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> N >> k;
+    scores = std::vector<int>(N);
+    tmp_arr = std::vector<int>(N);
+
+    for (int i = 0; i < N; ++i) {
+        std::cin >> scores[i];
+    }
+
+    sort(scores, 0, N);
+    std::cout << scores[k - 1] << "\n";
+
+    return 0;
+}
+
+void merge(std::vector<int> &v, int idx_s, int idx_m, int idx_e) {
+    int i, j, pos;
+
+    i = idx_s;
+    j = idx_m;
+    pos = idx_s;
+
+    while (i < idx_m && j < idx_e) {
+        if (v[i] > v[j]) {
+            tmp_arr[pos++] = v[i++];
+        } else {
+            tmp_arr[pos++] = v[j++];
+        }
+    }
+
+    while (i < idx_m) {
+        tmp_arr[pos++] = v[i++];
+    }
+
+    while (j < idx_e) {
+        tmp_arr[pos++] = v[j++];
+    }
+
+    for (i = idx_s; i < idx_e; ++i) {
+        v[i] = tmp_arr[i];
+    }
+}
+
+void sort(std::vector<int> &v, int idx_s, int idx_e) {
+    int idx_m;
+
+    if ((idx_e - idx_s) < 2)
+        return;
+
+    idx_m = idx_s + (idx_e - idx_s) / 2;
+    sort(v, idx_s, idx_m);
+    sort(v, idx_m, idx_e);
+    merge(v, idx_s, idx_m, idx_e);
+}
---
+++ 2750/2750.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+
+constexpr int MAX_NUM = 1001;
+
+int tmp[MAX_NUM];
+std::vector<int> arr;
+
+void merge(int idx_s, int idx_m, int idx_e);
+void sort(int idx_s, int idx_e);
+
+int main() {
+    int N;
+
+    // freopen("input.txt", "r", stdin);
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cin >> N;
+
+    arr = std::vector<int>(N);
+    for (int i = 0; i < N; ++i) {
+        std::cin >> arr[i];
+    }
+
+    sort(0, N);
+
+    for (int i : arr) {
+        std::cout << i << "\n";
+    }
+
+    return 0;
+}
+
+void merge(int idx_s, int idx_m, int idx_e) {
+    int i, j, pos;
+
+    i = idx_s;
+    j = idx_m;
+    pos = idx_s;
+
+    while (i < idx_m && j < idx_e) {
+        if (arr[i] < arr[j]) {
+            tmp[pos++] = arr[i++];
+        } else {
+            tmp[pos++] = arr[j++];
+        }
+    }
+
+    while (i < idx_m) {
+        tmp[pos++] = arr[i++];
+    }
+
+    while (j < idx_e) {
+        tmp[pos++] = arr[j++];
+    }
+
+    for (i = idx_s; i < idx_e; ++i) {
+        arr[i] = tmp[i];
+    }
+}
+
+void sort(int idx_s, int idx_e) {
+    int idx_m;
+
+    if ((idx_e - idx_s) < 2) {
+        return;
+    }
+
+    idx_m = (idx_e - idx_s) / 2 + idx_s;
+
+    sort(idx_s, idx_m);
+    sort(idx_m, idx_e);
+    merge(idx_s, idx_m, idx_e);
+}
