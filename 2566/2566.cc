5a1454c7c5c16978503929b717ac233ecf671a24
Wook Song <wook16.song@samsung.com>
2022-12-23 17:31:18 +0900

    Bronze: Add a solution for 2566 (Bronze II)

---
+++ 2566/2566.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+constexpr int LEN_MAT = 9;
+
+int main() {
+    std::vector<std::vector<int>> mat(LEN_MAT, std::vector<int>(LEN_MAT));
+    int r, c;
+    int max_val = INT32_MIN;
+
+    // freopen("input.txt", "r", stdin);
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    for (int i = 0; i < LEN_MAT; ++i) {
+        for (int j = 0; j < LEN_MAT; ++j) {
+            std::cin >> mat[i][j];
+            if (mat[i][j] > max_val) {
+                r = i + 1;
+                c = j + 1;
+                max_val = mat[i][j];
+            }
+        }
+    }
+
+    std::cout << max_val << "\n";
+    std::cout << r << " " << c << "\n";
+
+    return 0;
+}
