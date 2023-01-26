fbb6c1e19f497ed82332b159ccd8f4d07c048ebb
Wook Song <wook16.song@samsung.com>
2023-01-26 12:27:03 +0900

    Bronze: Add a solution for 2712 and 10810 (Bronze III)

---
+++ 10810/10810.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> buckets;
+    int N, M;
+
+    // freopen("input.txt", "r", stdin);
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    std::cin >> N >> M;
+    buckets = std::vector<int>(N + 1, 0);
+
+    for (int m = 0; m < M; ++m) {
+        int from, to, num;
+
+        std::cin >> from >> to >> num;
+
+        for (int i = from; i <= to; ++i) {
+            buckets[i] = num;
+        }
+    }
+
+    for (int n = 1; n <= N; ++n) {
+        std::cout << buckets[n] << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
---
+++ 2712/2712.cc
@@ -0,0 +1,45 @@
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+int main() {
+    std::stringstream ss;
+    std::string line;
+    std::string unit;
+    double val;
+    int N;
+
+    // freopen("input.txt", "r", stdin);
+
+    std::cin >> N;
+    std::cin.ignore();
+    for (int i = 0; i < N; ++i) {
+        std::string cunit;
+        double cval;
+
+        std::getline(std::cin, line);
+
+        ss.clear();
+        ss.str(line);
+        ss >> val >> unit;
+
+        if (unit == "kg") {
+            cunit = "lb";
+            cval = val * 2.2046;
+        } else if (unit == "lb") {
+            cunit = "kg";
+            cval = val * 0.4536;
+        } else if (unit == "l") {
+            cunit = "g";
+            cval = val * 0.2642;
+        } else if (unit == "g") {
+            cunit = "l";
+            cval = val * 3.7854;
+        }
+
+        std::cout << std::fixed << std::setprecision(4) << cval;
+        std::cout << " " + cunit << "\n";
+    }
+
+    return 0;
+}
