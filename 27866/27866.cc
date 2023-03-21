5b33e42b66347aba341c4c71c5ba735368093941
Wook Song <wook16.song@samsung.com>
2023-03-21 09:50:49 +0900

    Bronze: Add a solution for 27866 (Bronze V)

---
+++ 27866/27866.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+constexpr size_t MAX_LEN = 1001;
+
+int main() {
+    std::string str;
+    size_t pos;
+    char letter;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    str.reserve(MAX_LEN);
+    std::cin >> str;
+    std::cin >> pos;
+
+    letter = str[pos - 1];
+    std::cout << letter << "\n";
+
+    return 0;
+}
