a2cfc42c4fa31e9c7fe9ea5e470e4d9d3063fcc3
Wook Song <wook16.song@samsung.com>
2023-03-23 09:03:44 +0900

    Silver: Add a solution for 13241 (Silver V)

---
+++ 13241/13241.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using bigint_t = long long int;
+
+bigint_t gcd(bigint_t l, bigint_t r);
+bigint_t lcm(bigint_t l, bigint_t r, bigint_t gcd);
+
+int main() {
+    bigint_t A, B;
+
+    // freopen("input.txt", "r", stdin);
+    std::cin >> A >> B;
+
+    if (B > A) {
+        std::swap(A, B);
+    }
+
+    std::cout << lcm(A, B, gcd(A, B)) << "\n";
+
+    return 0;
+}
+
+bigint_t gcd(bigint_t l, bigint_t r) {
+    if (l % r == 0) {
+        return r;
+    }
+
+    return gcd(r, l % r);
+}
+
+bigint_t lcm(bigint_t l, bigint_t r, bigint_t gcd) { return (l / gcd) * r; }
