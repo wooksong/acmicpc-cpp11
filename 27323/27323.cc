bb3f8724f0de368ffb4399cc2553aaa218e2af78
Wook Song <wook16.song@samsung.com>
2023-03-24 16:35:48 +0900

    Bronze: Add a solution for 27323 (Bronze V)

---
+++ 27323/27323.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int len_horizontal, len_vertical;
+
+    // freopen("input.txt", "r", stdin);
+
+    std::cin >> len_horizontal >> len_vertical;
+    std::cout << len_horizontal * len_vertical << "\n";
+
+    return 0;
+}
